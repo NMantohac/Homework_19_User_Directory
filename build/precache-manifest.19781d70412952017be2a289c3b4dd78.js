3e21133075010954d546308f8e14df8fb4f69874
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-22 15:41:38 -0700

    ready to deploy app

---
+++ build/precache-manifest.19781d70412952017be2a289c3b4dd78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c5752555420b8c77086de256f77023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0e657677d0e87efb2e8",
+    "url": "/static/css/main.b19b2d15.chunk.css"
+  },
+  {
+    "revision": "7e3cf22f2cafe4aea489",
+    "url": "/static/js/2.3cdc95e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cdc95e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e657677d0e87efb2e8",
+    "url": "/static/js/main.7ef8a59a.chunk.js"
+  },
+  {
+    "revision": "aac9f68ca2e15ac0f3bf",
+    "url": "/static/js/runtime-main.49951ccb.js"
+  },
+  {
+    "revision": "e507875a5dd9b4ccdd4157262cd03b81",
+    "url": "/static/media/Header-Background.e507875a.jpg"
+  }
+]);
